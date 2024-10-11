a9c01c206c3b3c06872177c72f1b041f2f4f4202
kaio <kaio.devfull@gmail.com>
2024-10-11 03:06:19 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const showPasswordCheckbox = document.querySelector('#showPassword');
 const passwordField = document.querySelector('.senha');
+const passwordField2 = document.querySelector('.senha2')
 
 showPasswordCheckbox.addEventListener('change', function() {
     // Alterna entre mostrar e ocultar a senha
     const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordField.setAttribute('type', type);
+    const type2 = passwordField2.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField2.setAttribute('type', type);
 });
